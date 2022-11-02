09e4937be2e75497f7b0f4a5c6231ef99cc16f7a
BenceHargitai <beni.hargitai@gmail.com>
2022-11-02 08:39:47 +0100

    10.gyakorlat feladatai

--- 2022.11.02. - Gyakorlat/Haskell/Lesson.hs
+++ 2022.11.02. - Gyakorlat/Haskell/Lesson.hs
@@ -1,1 +1,53 @@
 module Lesson where
+import Data.Char
+
+-- *? 10.gyakorlat
+
+any' :: (a->Bool) -> [a] -> Bool
+any' _ [] = False
+any' f (x:xs)
+    | not (f x) = any' f xs
+    | otherwise = True
+
+
+hasLongLines :: String -> Bool
+hasLongLines "" = False
+hasLongLines s = any' (\x -> length x >= 3) (words s)
+
+elem' :: Eq a => a -> [a] -> Bool
+elem' c [] = False
+elem' c s = any' (\x -> x == c) s
+
+hasAny :: Eq a => [a] -> [a] -> Bool
+hasAny s [] = False
+hasAny [] s = False
+hasAny (x:xs) s = any' (\y -> y == x) s
+
+takeWhile' :: (a->Bool) -> [a] -> [a]
+takeWhile' f [] = []
+takeWhile' f (x:xs)
+    | f x = x : takeWhile' f xs
+    | otherwise = []
+
+dropWhile' :: (a->Bool) -> [a] -> [a]
+dropWhile' f [] = []
+dropWhile' f (x:xs)
+    | f x = dropWhile' f xs
+    | otherwise = x:xs
+
+
+dropWord :: String -> String
+dropWord "" = ""
+dropWord s
+    | length (words s) > 1 = dropWhile' (\x -> x /= ' ') s
+    | otherwise = s
+
+users :: [(String, String)]
+users = [ ("mrbean", "4321"), ("admin", "s3cr3t"), ("finn", "algebraic")]
+
+doesUserExist :: String -> [(String, String)] -> Bool
+doesUserExist "" _ = False
+doesUserExist _ [] = False
+doesUserExist s (x:xs)
+    | s == fst x = True
+    | otherwise = doesUserExist s xs
