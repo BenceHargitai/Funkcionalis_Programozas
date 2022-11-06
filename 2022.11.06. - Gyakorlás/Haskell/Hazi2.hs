20814a62e02c160b6afb4045483b71df8493d055
BenceHargitai <beni.hargitai@gmail.com>
2022-11-06 13:39:35 +0100

    decodeCaesar

--- 2022.11.06. - Gyakorlás/Haskell/Hazi2.hs
+++ 2022.11.06. - Gyakorlás/Haskell/Hazi2.hs
@@ -8,12 +8,18 @@ mapping :: [(Char,Char)]
 mapping  = zip osszes (drop 3 [x | x <- osszes] ++ take 3 [x | x <- osszes])
 
 encodeCaesar :: String -> String
-encodeCaesar s = map (\x -> 
+encodeCaesar = map (\x -> 
     if length (filter (\(a,b) -> a == x) mapping) > 0 then 
         snd (filter (\(a,b) -> a == x) mapping !! 0) 
     else 
-        '?'
-) s 
-decodeCaesar ::
+        '?')
+
+
+decodeCaesar ::String -> String
+decodeCaesar = map (\x -> 
+    if length (filter (\(a,b) -> b == x) mapping) > 0 then 
+        fst (filter (\(a,b) -> b == x) mapping !! 0) 
+    else 
+        '?') 
 
 
