500b03d42f8d5579e9e7d9ddf622b315f5f19296
BenceHargitai <beni.hargitai@gmail.com>
2022-11-08 20:22:51 +0100

    11-12 feladatsor

--- 2022.11.08. - Gyakorlás/Haskell/Lesson.hs
+++ 2022.11.08. - Gyakorlás/Haskell/Lesson.hs
@@ -1,5 +1,6 @@
 module Lesson where
 import Data.Char
+import Data.List
 
 map' :: (a -> b) -> [a] -> [b]
 map' f [] = []
@@ -74,4 +75,44 @@ trim s = reverse $ dropSpaces $ reverse $ dropSpaces s
 
 monogram :: String -> String
 monogram "" = ""
-monogram 
+monogram s = unwords $ map (\(x:xs) -> x : ".") (words s)
+
+uniq :: Ord a => [a] -> [a]
+uniq [] = []
+uniq l = map(\(x:xs) -> x) $ group $ sort l 
+
+repeated :: Ord a => [a] -> [a]
+repeated [] = []
+repeated l = map(\(x:xs) -> x) $ filter(\y -> length y > 1) $ group $ sort l
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' f [] list2 = []
+zipWith' f list1 [] = []
+zipWith' f (a:as) (b:bs) = f a b : zipWith' f as bs
+
+-- --zipwith high order function
+-- zipWith'' :: (a -> b -> c) -> [a] -> [b] -> [c]
+-- zipWith'' f [] list2 = []
+-- zipWith'' f list1 [] = []
+-- zipWith'' f list1 list2 = foldr (\(x,y) acc -> f x y : acc) [] (zip list1 list2)
+
+dotProduct :: [Int] -> [Int] -> Int
+dotProduct [] l2 = 0
+dotProduct l1 [] = 0
+dotProduct l1 l2 = sum $ zipWith' (*) l1 l2
+
+isPrime :: Int -> Bool
+isPrime 1 = False
+isPrime a 
+    | length (filter(\x -> x == True) (map(\x -> a `mod` x == 0) [2..a-1])) == 0 = True 
+    | otherwise = False
+
+primes :: Int -> [Int]
+primes x = [l | l <- [2..x], isPrime l]
+
+iterate'' :: (a -> a) -> a -> [a] 
+iterate'' f x = x : iterate'' f (f x)
+
+fibonacci :: [Int]
+fibonacci = 0 : 1 : zipWith' (+) fibonacci (tail fibonacci)
+
